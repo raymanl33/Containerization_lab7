c22cf13460a8e3e396424bfbefbf77361faaf4d9
Raymond Lee <raymanl33@icloud.com>
2022-11-12 13:45:14 -0800

    test

--- receiver/app.py
+++ receiver/app.py
@@ -33,81 +33,81 @@ while current_count < maximum_retries:
       client = KafkaClient(hosts=f"{app_config['events']['hostname']}:{app_config['events']['port']}")
       topic = client.topics[str.encode(app_config['events']['topic'])]
 
+      def book_tennis_court(body):
 
-      
+        """ Recevies a tennis booking request """
+        trace_id = uuid.uuid4()
+
+        logger.info(f'Received book tennis lesson court event request with a trace id of {str(trace_id)}')
+        url = app_config['eventstore1']['url']
+
+        tennis_booking_data = {
+        "bookDate": body['bookDate'],
+        "courtNum": body['courtNum'],
+        "memberID": body['memberID'],
+        "memberName": body['memberName'],
+        "trace_id" : str(trace_id)
+        }
+
+        producer = topic.get_sync_producer()
+
+        msg = {"type": "book_tennis_court", 
+        "datetime": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"), 
+        "payload": tennis_booking_data }
+        msg_str = json.dumps(msg)
+        producer.produce(msg_str.encode('utf-8'))
+
+        # hard code status code to 201
+        response = 201
+
+        if response == 201:
+          logger.info(f'Returned event book tennis lesson response {str(trace_id)} with status {response}')
+
+
+        return NoContent, 201
+
+
+
+      def book_tennis_lesson(body):
+        """ Receives a tennis booking lesson request """
+        trace_id = uuid.uuid4()
+        logger.info(f'Received event book tennis lesson request with a trace id of {str(trace_id)}')
+        url = app_config['eventstore2']['url']
+
+
+        producer = topic.get_sync_producer()
+
+        tennisLesson_booking_data = {
+        "coachName":  body['coachName'],
+        "lessonDate":  body['lessonDate'],
+        "lessonRate":  body['lessonRate'],
+        "memberID":  body['memberID'],
+        "memberName":  body['memberName'],
+        "trace_id" : str(trace_id)
+        }
+
+        msg = {"type": "book_tennis_lesson", 
+        "datetime": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"), 
+        "payload": tennisLesson_booking_data }
+        msg_str = json.dumps(msg)
+        producer.produce(msg_str.encode('utf-8'))
+        
+        # hard code status code to 201
+        response = 201
+        
+        if response == 201:
+          logger.info(f'Returned event book tennis lesson response {str(trace_id)} with status {response}')
+
+
+        return NoContent, 201
+            
     
   except Exception as e:
     logger.error(f"Connection to Kafka Failed {e}")
     time.sleep(sleep_time)
     current_count += 1
 
-def book_tennis_court(body):
 
-  """ Recevies a tennis booking request """
-  trace_id = uuid.uuid4()
-
-  logger.info(f'Received book tennis lesson court event request with a trace id of {str(trace_id)}')
-  url = app_config['eventstore1']['url']
-
-  tennis_booking_data = {
-  "bookDate": body['bookDate'],
-  "courtNum": body['courtNum'],
-  "memberID": body['memberID'],
-  "memberName": body['memberName'],
-  "trace_id" : str(trace_id)
-  }
-
-  producer = topic.get_sync_producer()
-
-  msg = {"type": "book_tennis_court", 
-  "datetime": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"), 
-  "payload": tennis_booking_data }
-  msg_str = json.dumps(msg)
-  producer.produce(msg_str.encode('utf-8'))
-
-  # hard code status code to 201
-  response = 201
-
-  if response == 201:
-    logger.info(f'Returned event book tennis lesson response {str(trace_id)} with status {response}')
-
-
-  return NoContent, 201
-
-
-
-def book_tennis_lesson(body):
-  """ Receives a tennis booking lesson request """
-  trace_id = uuid.uuid4()
-  logger.info(f'Received event book tennis lesson request with a trace id of {str(trace_id)}')
-  url = app_config['eventstore2']['url']
-
-
-  producer = topic.get_sync_producer()
-
-  tennisLesson_booking_data = {
-  "coachName":  body['coachName'],
-  "lessonDate":  body['lessonDate'],
-  "lessonRate":  body['lessonRate'],
-  "memberID":  body['memberID'],
-  "memberName":  body['memberName'],
-  "trace_id" : str(trace_id)
-  }
-
-  msg = {"type": "book_tennis_lesson", 
-  "datetime": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"), 
-  "payload": tennisLesson_booking_data }
-  msg_str = json.dumps(msg)
-  producer.produce(msg_str.encode('utf-8'))
-  
-  # hard code status code to 201
-  response = 201
-  
-  if response == 201:
-    logger.info(f'Returned event book tennis lesson response {str(trace_id)} with status {response}')
-
-
-  return NoContent, 201
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api("openapi.yaml",
