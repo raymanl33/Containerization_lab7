44f899241e52773bc0117d4b6d3ddab4db068668
Raymond Lee <raymanl33@icloud.com>
2022-11-22 13:08:43 -0800

    test

---
+++ dashboard-ui/src/components/HealthStats.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react'
+import '../App.css';
+
+export default function HealthStats() {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [stats, setStats] = useState({});
+    const [error, setError] = useState(null)
+
+	const getStats = () => {
+	
+        fetch(`http://kafka.canadacentral.cloudapp.azure.com:8120/health`)
+            .then(res => res.json())
+            .then((result)=>{
+				console.log("Received Stats")
+                setStats(result);
+                setIsLoaded(true);
+            },(error) =>{
+                setError(error)
+                setIsLoaded(true);
+            })
+    }
+    useEffect(() => {
+		const interval = setInterval(() => getStats(), 2000); // Update every 2 seconds
+		return() => clearInterval(interval);
+    }, [getStats]);
+
+    if (error){
+        return (<div className={"error"}>Error found when fetching from API</div>)
+    } else if (isLoaded === false){
+        return(<div>Loading...</div>)
+    } else if (isLoaded === true){
+        return(
+            <div>
+                <h1>Latest Stats</h1>
+                <table className={"StatsTable"}>
+					<tbody>
+						<tr>
+							<th>Health Check</th>
+						</tr>
+						<tr>
+							<td>Audit: {stats['audit']}</td>
+							<td>Processing: {stats['processing']}</td>
+							<td>Receiver {stats['receiver']}</td>
+							<td>Storage {stats['storage']}</td>
+						</tr>
+					
+					</tbody>
+                </table>
+                <h3>Last Updated: {stats['Last_update']}</h3>
+
+            </div>
+        )
+    }
+}
