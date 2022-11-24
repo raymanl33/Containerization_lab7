782f27af6dcf01cd0b8a8016e0ce0bc5e40638e6
Raymond Lee <raymanl33@icloud.com>
2022-11-23 22:45:02 -0800

    test

--- audit_log/app.py
+++ audit_log/app.py
@@ -114,12 +114,13 @@ def get_tennis_lessons_reading(index):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("openapi.yaml",
+app.add_api("openapi.yaml", base_path="/audit_log",
                  strict_validation=True,
                 validate_responses=True)
 
-CORS(app.app)
-app.app.config["CORS_HEADERS"] = "Content-Type"
+if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test":
+    CORS(app.app)
+    app.app.config["CORS_HEADERS"] = "Content-Type"
 
 if __name__ == "__main__":
     app.run(port=9080)
--- receiver/app.py
+++ receiver/app.py
@@ -127,7 +127,7 @@ def book_tennis_lesson(body):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("openapi.yaml",
+app.add_api("openapi.yaml", base_path="/receiver",
                  strict_validation=True,
                 validate_responses=True)
 
